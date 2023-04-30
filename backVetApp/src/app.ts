cfab6c1d8ecb5ac9e923bdec2b79ba71c76c7cfb
marianog <marianogonzalezazcon@gmail.com>
2023-04-30 14:05:18 -0300

    mejorando la estructura del backend, para hacerlo mas escalable

---
+++ backVetApp/src/app.ts
@@ -0,0 +1,29 @@
+import { Request, Response, Router } from "express"
+
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const PORT = process.env.port || 3000
+
+//importacion de rutas, mas adelante se cambia
+import { AdopcionesRouter } from "./routes/adopciones"
+import { ApiResponse } from "./interfaces/ApiResponse.interface"
+
+
+app.use(AdopcionesRouter);
+
+app.get('/',cors(), (req:Request, res:Response) => {
+	res.send('Test backend')
+})
+
+app.get('/api/mensaje',cors(), (req:Request, res:Response) => {
+  const response:ApiResponse<string> = {
+    data:'Mensaje desde el backend',
+    statusCode:200,
+  }
+  res.send(response)
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
