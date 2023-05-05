1edbd8f1c6bb78849b52bb9a8b9fd52bb9fd6f6a
Felipe Tamburri Merlo <felipetamburri@gmail.com>
2023-05-05 13:55:29 -0300

    Empiezo con paseadores y cuidadores

--- frontVetApp/src/routes/BotonConDatos.svelte
+++ frontVetApp/src/routes/BotonConDatos.svelte
@@ -2,14 +2,16 @@
 	import { getTestData } from "$lib/dataFetching/dataFetching";
 	import type { ApiResponse } from "$lib/interfaces/ApiResponse.interface";
 
-	let datos:ApiResponse = {data:"noData",statusCode:-1};
-	let endpoint:string = 'http://localhost:3000/api/mensaje';
+	let datos: ApiResponse = { data: "noData", statusCode: -1 };
+	let endpoint: string = "http://localhost:3000/api/mensaje";
 
-	async function getDatos(){
+	async function getDatos() {
 		datos = await getTestData(endpoint);
 	}
-
 </script>
 
-<button type="button" class="btn rounded-lg variant-filled" on:click={getDatos}>Hacer llamada get</button>  
+<button type="button" class="btn rounded-lg variant-filled" on:click={getDatos}
+	>Hacer llamada get</button
+>
 <h1>Datos: {datos.data}</h1>
+<a href="/paseadores-y-cuidadores">Paseadores y Cuidadores</a>
