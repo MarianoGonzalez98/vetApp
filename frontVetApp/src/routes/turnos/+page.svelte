632f9d679c4f641b3a33364391a2a5a7953645c6
Julia <julia.rojas195060@alumnos.info.unlp.edu.ar>
2023-05-06 23:44:46 -0300

    probando tablas

--- frontVetApp/src/routes/turnos/+page.svelte
+++ frontVetApp/src/routes/turnos/+page.svelte
@@ -1,34 +1,58 @@
 <script>
+    import { AppBar } from "@skeletonlabs/skeleton";
     import TurnoForm from "./TurnoForm.svelte";
 
+    const tableArr = [
+        {turno: "Uno", motivo: "Castracion", fecha: "1/2/2023", rangoHor: "Mañana" },
+        {turno: "Dos", motivo: "Vacunacion", fecha: "1/5/2023", rangoHor: "Mañana" },
+        {turno: "Tres", motivo: "Vacunacion", fecha: "1/9/2023", rangoHor: "Tarde" },
+        {turno: "Cuatro", motivo: "Consulta General", fecha: "1/10/2023", rangoHor: "Noche"}
+        ];
+
 </script>
-<!-- <script lang="ts"> 
-    import type { Turno } from "$lib/interfaces/Turno.interface";
-</script>
-    
-    <style>
-      .content {
-        display: grid;
-        grid-template-columns: 20% 80%;
-        grid-column-gap: 10px;
-      }
-    </style>
-    
-    <h1>Form</h1>
-    <form class="content">
-      <label>Motivo</label>
-      <input type="text" bind:value={$Turno.motivo} />
-      <label>Perro</label>
-      <input type="text" bind:value={$Turno.} />
-      <label>Fecha</label>
-      <input type="text" bind:value={$user.phone} />
-      <label>Rango Horario</label>
-      <input type="text" bind:value={$user.phone} />
-    </form>
-    <p>
-        {JSON.stringify($user, 0, 2)}</p>
 
 
-<h1> Turnos </h1> -->
+<br>
+<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+	<svelte:fragment slot="lead"></svelte:fragment>
+	<h1>Turnos</h1>
+	<svelte:fragment slot="trail"></svelte:fragment>
+</AppBar>
+
+<br>
+<br>
+
+
+<!-- Responsive Container (recommended) -->
+<div class="table-container">
+	<!-- Native Table Element -->
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				<th>Turno</th>
+				<th>Motivo</th>
+				<th>Fecha</th>
+				<th>Rango Horario</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each tableArr as row, i}
+				<tr>
+					<td>{row.turno}</td>
+					<td>{row.motivo}</td>
+					<td>{row.fecha}</td>
+					<td>{row.rangoHor}</td>
+				</tr>
+			{/each}
+		</tbody>
+		<!-- <tfoot>
+			<tr>
+				<th colspan="3">Calculated Total Weight</th>
+				<td>{totalWeight}</td>
+			</tr>
+		</tfoot> -->
+	</table>
+</div>
+
 
 <TurnoForm></TurnoForm>
