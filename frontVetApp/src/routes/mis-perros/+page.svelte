3148f27d31dd43b78b3dbcfe5f3c99f361c81149
Felipe Tamburri Merlo <felipetamburri@gmail.com>
2023-05-10 00:03:06 -0300

    Empiezo a trabajar con Cargar perro

---
+++ frontVetApp/src/routes/mis-perros/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import type { PageData } from "./$types";
+
+    export let data: PageData;
+</script>
+
+<h1>Mis perros</h1>
+<a
+    class="btn btn-sm variant-ghost-surface"
+    rel="noreferrer"
+    href="/modificar-perro">Modificar perro</a
+>
+<button class="btn btn-sm variant-ghost-surface"
+    >Marcar perro como fallecido</button
+>
