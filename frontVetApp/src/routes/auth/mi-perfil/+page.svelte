168f7bb9690b07840ec32d8f6a493e9a54286093
marianog <marianogonzalezazcon@gmail.com>
2023-05-11 17:24:25 -0300

    empezando mi-perfil

---
+++ frontVetApp/src/routes/auth/mi-perfil/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+
+
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+    <div>
+        <div>
+            <p class=" text-2xl ">Estoy viendo los datos de mi perfil</p>
+            <p>Nombre: </p>
+            <p>Apellido: </p>
+            <p>Email: </p>
+            <p>DNI: </p>
+            <p>Telefono: </p>
+        </div>
+
+        <a
+        class="btn variant-ghost-surface"
+        rel="noreferrer"
+        href="/auth/cambiar-password">Cambiar mi contraseña</a
+        >
+    </div>
+
+</div>
+
+<style>
+    
+</style>
