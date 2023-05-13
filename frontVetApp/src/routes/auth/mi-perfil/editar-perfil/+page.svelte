03fe6f87796e3757baccf20e853c8cd2a95dd93f
marianog <marianogonzalezazcon@gmail.com>
2023-05-13 01:51:37 -0300

    funcionalidad completa ver y editar mi perfil

--- frontVetApp/src/routes/auth/mi-perfil/editar-perfil/+page.svelte
+++ frontVetApp/src/routes/auth/mi-perfil/editar-perfil/+page.svelte
@@ -7,36 +7,46 @@
         type PopupSettings,
     } from "@skeletonlabs/skeleton";
     import { Modal, modalStore } from "@skeletonlabs/skeleton";
+    import { error } from "@sveltejs/kit";
     import { onMount } from "svelte";
-    import { FileDropzone } from '@skeletonlabs/skeleton';
 
+    
+    let nombre = "";
+    let apellido = "";
+    let dni = "";
+    let direccion = "";
+    let telefono = "";
+    let fechaNacimiento: string; //
+    let foto:any;
+
+    let FotoFile: FileList; //por ahora ni tiene utilidad
     let submittedClass = "";
-    const emailPattern: string =
-        "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+[.][a-zA-Z]{2,}$";
+    let fechaMax: string = new Date().toJSON().slice(0, 10);
+
     const letrasEspaciosPattern: string =
         "^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ]+$";
 
     const numbersPattern: string = "^[0-9]*$";
     let emailErrorMsj = "";
-
+    let fileErrorMsj = "";
     const clienteCargado: ModalSettings = {
         type: "alert",
-        title: "Cliente cargado",
-        body: "Cliente registrado correctamente",
+        title: "Perfil actualizado",
+        body: "Perfil actualizado correctamente",
         buttonTextCancel: "Ok",
-        response: (r: boolean) => goto("/"),
+        response: (r: boolean) => goto("/auth/mi-perfil"),
     };
 
     const fallaDesconocida: ModalSettings = {
         type: "alert",
         title: "Error desconocido",
-        body: "No se pudo registrar el nuevo cliente",
+        body: "No se pudo actualizar el perfil",
         buttonTextCancel: "Ok",
     };
 
     const fallaServidor: ModalSettings = {
         type: "alert",
-        title: "Fallo de la carga del cliente",
+        title: "Fallo en actualizacion del perfil",
         body: "Falla del servidor",
         buttonTextCancel: "Ok",
     };
@@ -47,39 +57,67 @@
         placement: "top",
     };
 
-    let nombre = "";
-    let apellido = "";
-    let email = "";
-    let dni = "";
-    let direccion = "";
-    let telefono = "";
-    let fechaNacimiento: string; //
-    let fechaMax: string = new Date().toJSON().slice(0, 10);
-    let FotoFile: FileList; //por ahora ni tiene utilidad
+    const eliminarFoto = () => {
+        foto="";
+    }
 
-    let avatar:any;
+    onMount( () => {
+        fetch('http://localhost:3000/getPerfil',{
+            method:'GET',
+            headers:{
+                'Content-Type':'application/json',
+            },
+            credentials: 'include',
+        }).then( (res) => {
+            if (res.status < 299) {  //si entra acá no hubo error
+                return res.json()
+            }
+            return Promise.reject(res);
+        }).then( (res) => {
+            nombre=res.nombre;
+            apellido=res.apellido;
+            dni=res.dni;
+            direccion=res.direccion;
+            telefono=res.telefono;
+            fechaNacimiento=res.fechaNacimiento.slice(0, 10); //despues capaz tenga que cambiar el formato??
+            foto=res.foto;
+        }).catch( (e)  => {
+            console.log("ERROR:");
+            console.log(e);
+        })
+    })
 
-// then in the function
     const onChangeFile = async (event: Event) => {
-        const target = event.target as HTMLInputElement;
+        let target = event.target as HTMLInputElement;
         if (!target.files){
             return
         }
         let image = target.files[0];
+        if (image.size > 5242880){ // 5 mb en bytes
+            fileErrorMsj = "La imagen debe pesar menos de 5 MB"
+            target.value="";
+            return
+        }
+        if ((image.type !== 'image/jpeg') && (image.type!== 'image/png') ){
+            fileErrorMsj = "El archivo debe ser una imagen jpg, jpeg o png"
+            target.value="";
+            return
+        }
+        fileErrorMsj ="";
         let reader = new FileReader();
         reader.readAsDataURL(image);
         reader.onload = e => {
             if (!e.target){
                 return
             }
-            avatar = e.target.result
-            console.log(avatar);
+            foto = e.target.result
+            //console.log(foto);
         };
     }
 
     const handleCarga = () => {
-        fetch("http://localhost:3000/registrar-cliente", {
-            method: "POST",
+        fetch("http://localhost:3000/updatePerfil", {
+            method: "PUT",
             headers: {
                 "Content-Type": "application/json",
             },
@@ -87,12 +125,11 @@
             body: JSON.stringify({
                 nombre,
                 apellido,
-                email,
                 dni,
                 direccion,
                 telefono,
                 fechaNacimiento,
-                foto: null,
+                foto: foto,
             }),
         })
             .then((res) => {
@@ -104,11 +141,11 @@
                 if (res.status === 400) {
                     //error por modificacion del token jwt.
                     $user = null;
-                    goto("/auth/login");
+                    goto('/auth/login');
                     return;
                 }
-                if (res.status === 409) {
-                    emailErrorMsj = "El email ya se encuentra registrado";
+                if (res.status === 404) {
+                    console.log("El usuario no existe...");
                     return res;
                 }
                 if (res.status === 500) {
@@ -120,18 +157,10 @@
             .catch((error) => {
                 modalStore.clear();
                 modalStore.trigger(fallaDesconocida);
-                console.log("Error en carga del cliente desconocido: ", error);
+                console.log("Error desconocido: ", error);
             });
     };
 
-    onMount( () => {
-        nombre="Mi nombre";
-        apellido = "Mi apellido";
-        dni = "1234556";
-        direccion = "Mi direccion";
-        telefono = "221454256";
-        fechaNacimiento = new Date().toJSON().slice(0, 10);
-    } )
 
 </script>
 
@@ -142,92 +171,98 @@
 >
     <form
         on:submit|preventDefault={handleCarga}
-        class="space-y-2 {submittedClass}"
+        class=" {submittedClass}"
     >
-        <label class="label" for="nombre">Nombre:</label>
-        <input
-            bind:value={nombre}
-            class="input focus:invalid:border-red-500"
-            type="text"
-            placeholder="Ingrese su nombre"
-            name="nombre"
-            pattern={letrasEspaciosPattern}
-            required
-        />
-
-        <label class="label" for="apellido">Apellido:</label>
-        <input
-            bind:value={apellido}
-            class="input focus:invalid:border-red-500"
-            type="text"
-            placeholder="Ingrese su apellido"
-            name="apellido"
-            pattern={letrasEspaciosPattern}
-            required
-        />
-
-        <label class="label" for="dni">Teléfono:</label>
-        <input
-            bind:value={telefono}
-            class="input focus:invalid:border-red-500"
-            type="text"
-            placeholder="Ingrese teléfono del cliente. Ej: 2214687634"
-            use:popup={popupFocusBlur}
-            name="telefono"
-            pattern={numbersPattern}
-            required
-        />
-
-        <div class="card p-4 variant-filled" data-popup="popupFocusBlur">
-            <p>Sólo números</p>
-            <div class="arrow variant-filled" />
-        </div>
-
-        <label class="label" for="dni">DNI:</label>
-        <input
-            bind:value={dni}
-            class="input focus:invalid:border-red-500"
-            type="text"
-            max="9999999999"
-            placeholder="Ingrese dni del cliente"
-            name="dni"
-            autocomplete="off"
-            pattern={numbersPattern}
-            required
-        />
-
-        <label class="label" for="fechaNacimiento">Fecha de nacimiento:</label>
-        <input
-            bind:value={fechaNacimiento}
-            class="input"
-            type="date"
-            placeholder="Ingrese fecha de nacimiento del cliente"
-            name="fechaNacimiento"
-            max={fechaMax}
-            required
-        />
-
-        <label class="label" for="direccion">Dirección:</label>
-        <input
-            bind:value={direccion}
-            class="input focus:invalid:border-red-500"
-            type="text"
-            placeholder="Ingrese dirección del cliente"
-            name="direccion"
-            required
-        />
-        <p>Foto:</p> 
-        <input bind:files={FotoFile} type="file" accept="image/png, image/jpeg"
-            on:change={onChangeFile}>
+        <div class="space-y-1 mb-10">
+            <label class="label" for="nombre">Nombre:</label>
+            <input
+                bind:value={nombre}
+                class="input focus:invalid:border-red-500"
+                type="text"
+                placeholder="Ingrese su nombre"
+                name="nombre"
+                pattern={letrasEspaciosPattern}
+                required
+            />
+    
+            <label class="label" for="apellido">Apellido:</label>
+            <input
+                bind:value={apellido}
+                class="input focus:invalid:border-red-500"
+                type="text"
+                placeholder="Ingrese su apellido"
+                name="apellido"
+                pattern={letrasEspaciosPattern}
+                required
+            />
+    
+            <label class="label" for="dni">Teléfono:</label>
+            <input
+                bind:value={telefono}
+                class="input focus:invalid:border-red-500"
+                type="text"
+                placeholder="Ingrese teléfono del cliente. Ej: 2214687634"
+                use:popup={popupFocusBlur}
+                name="telefono"
+                pattern={numbersPattern}
+                required
+            />
+    
+            <div class="card p-4 variant-filled" data-popup="popupFocusBlur">
+                <p>Sólo números</p>
+                <div class="arrow variant-filled" />
+            </div>
+    
+            <label class="label" for="dni">DNI:</label>
+            <input
+                bind:value={dni}
+                class="input focus:invalid:border-red-500"
+                type="text"
+                max="9999999999"
+                placeholder="Ingrese dni del cliente"
+                name="dni"
+                autocomplete="off"
+                pattern={numbersPattern}
+                required
+            />
+    
+            <label class="label" for="fechaNacimiento">Fecha de nacimiento:</label>
+            <input
+                bind:value={fechaNacimiento}
+                class="input"
+                type="date"
+                placeholder="Ingrese fecha de nacimiento del cliente"
+                name="fechaNacimiento"
+                max={fechaMax}
+                required
+            />
+    
+            <label class="label" for="direccion">Dirección:</label>
+            <input
+                bind:value={direccion}
+                class="input focus:invalid:border-red-500"
+                type="text"
+                placeholder="Ingrese dirección del cliente"
+                name="direccion"
+                required
+            />
+            <span>Foto:</span>
+            <button on:click={eliminarFoto} class="btn rounded btn-sm variant-filled-warning" type="button">Eliminar foto</button>
             <div>
-                {#if avatar}
-                    <img class="object-contain h-32 w-32" src="{avatar}" alt="foto de perfil" />
+                {#if foto}
+                    <img class="object-contain h-32 w-32" src="{foto}" alt="foto de perfil" />
                 {:else}
                     <img class="object-contain h-32 w-32" src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png" alt="" /> 
                 {/if}
             </div>
+            <p class="text-red-500">{fileErrorMsj}</p>
+            <input bind:files={FotoFile} type="file" accept="image/png, image/jpeg"
+                on:change={onChangeFile}>
+        </div>
+
+        <a href="/auth/mi-perfil/"><button class="btn rounded-lg variant-filled-secondary">Cancelar edición</button></a>
         <button class="btn rounded-lg variant-filled-primary" type="submit"
-            >Actualizar</button
+            >Actualizar mi perfil</button
         >
     </form>
 </div>
