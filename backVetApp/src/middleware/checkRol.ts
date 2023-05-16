bf3d180f2b72a356498fa5d7c126592610d5ddcf
felipetamburrimerlo <felipetamburri@gmail.com>
2023-05-16 16:39:32 -0300

    Termino Cargar paseador/cuidador

--- backVetApp/src/middleware/checkRol.ts
+++ backVetApp/src/middleware/checkRol.ts
@@ -12,18 +12,6 @@ export const checkRol = async (req: Request, res: Response, next: NextFunction) 
 
 }
 
-export const checkEsUsuario = async (req: Request, res: Response, next: NextFunction) => {
-    //console.log(res.locals.jwtData)
-    if (res.locals.jwtData.user.rol === 'cliente') {
-        next();
-
-    } else {
-        res.status(403);
-        res.send('No tiene permisos para esta operacion.');
-    }
-
-}
-
 export const checkRolCliente = async (req:Request, res:Response, next: NextFunction) => {
     //console.log(res.locals.jwtData)
     if (res.locals.jwtData.user.rol === 'cliente'){
--- backVetApp/src/routes/perros.routes.ts
+++ backVetApp/src/routes/perros.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express"
 import { cargarPerroController, listarPerrosController } from "../controllers/perros"
 import { checkJWT } from "../middleware/session"
-import { checkEsUsuario, checkRol } from "../middleware/checkRol"
+import { checkRolCliente, checkRol } from "../middleware/checkRol"
 
 export const PerrosRouter = Router();
 
 PerrosRouter.post("/cargar-perro", checkJWT, cargarPerroController);
-PerrosRouter.get("/listar-perros", checkJWT, checkEsUsuario, listarPerrosController);
+PerrosRouter.get("/listar-perros", checkJWT, checkRolCliente, listarPerrosController);
