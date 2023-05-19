f2b30e170585638a9da44ec8370b8a4204e1eb5f
Felipe Tamburri Merlo <felipetamburri@gmail.com>
2023-05-18 21:11:28 -0300

    Incorporo Ver clientes

--- frontVetApp/src/lib/interfaces/Cliente.interface.ts
+++ frontVetApp/src/lib/interfaces/Cliente.interface.ts
@@ -1,5 +1,13 @@
 export interface Cliente {
-    nombre:string
-    apellido:string
-    email:string
+    nombre: string
+    apellido: string
+    email: string
+}
+
+export interface ExtrasCliente {
+    dni: string
+    fechaNacimiento: string
+    direccion: string
+    telefono: string
+    foto: any
 }
