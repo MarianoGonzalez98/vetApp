5a80e37ea2885789923a355af4070ce5e9ff6f78
marianog <marianogonzalezazcon@gmail.com>
2023-05-30 22:17:15 -0300

    avanzando en donacion

--- backVetApp/src/controllers/mecadoPago.ts
+++ backVetApp/src/controllers/mecadoPago.ts
@@ -1,18 +1,30 @@
 import { Request, Response } from "express";
 import * as mercadopago from "mercadopago";
 import { Campaign } from "../interfaces/Donaciones.interface";
-//const mercadopago = require("mercadopago");
+import { sendMailWithAttachment } from "../utils/mailer.handle";
+import { generatePDF } from "../utils/pdf.handle";
+
+const ngrokURL= 'https://b3de-186-127-125-154.ngrok-free.app'; // acá hay que colocar la url que da ngrok en el momento.
+// comando: ngrok http 3000
 
 export const createPrefrerenceDonacionController = async (req: Request, res: Response) => {
-    console.log("Configurando")
+    console.log("Configurando MercadoPAgo")
     mercadopago.configure({
         access_token: process.env.MP_ACCESS_TOKEN || "",
     });
     const monto:number = req.body.monto;
     const campaign:Campaign = req.body.campaign;
+    const emailDonante:string = req.body.emailDonante;
 
+    console.log("Cuerpo recibido por la createPreferenceDonacionController: ");
     console.log(req.body);
-    console.log(req.body);
+
+    //preparo la url de la notificacion junto con los datos que necesitaré para identificarla
+    var donacionNotificationUrl = new URL(ngrokURL+"/notificacion_mp_donacion");
+    donacionNotificationUrl.searchParams.append("campaignNombre",campaign.nombre);
+    donacionNotificationUrl.searchParams.append('emailDonante',emailDonante)
+
+    console.log(donacionNotificationUrl);
     let preference = {
         items: [
             {
@@ -26,7 +38,7 @@ export const createPrefrerenceDonacionController = async (req: Request, res: Res
             "failure": "http://localhost:5173/donaciones",
             "pending": "http://localhost:5173/donaciones"
         },
-        notification_url:`https://36a6-186-127-125-154.ngrok-free.app/notificacion_mp_donacion`, //cambiar url de ngrok
+        notification_url: donacionNotificationUrl.toString(), //cambiar url de ngrok
     };
 
     mercadopago.preferences.create(preference)
@@ -42,15 +54,19 @@ export const createPrefrerenceDonacionController = async (req: Request, res: Res
 
 
 export const notificacionDonacionController = async (req: Request, res: Response) => {
+
+    console.log("QUERY de notificacion::")
+    console.log(req.query);
+    const emailDonante= req.query.emailDonante as string;
+    const campaignNombre = req.query.campaignNombre;
     console.log("Configurando")
     mercadopago.configure({
         access_token: process.env.MP_ACCESS_TOKEN || "",
     });
     
-    console.log("NOTIFICACION MP:");
+    console.log("NOTIFICACION MP:::::::::::::::::::::");
     const {body, query} = req;
     console.log({body,query});
-
     const topic = query.topic || query.type;
     console.log("TOPIC:");
     console.log({topic});
@@ -58,13 +74,42 @@ export const notificacionDonacionController = async (req: Request, res: Response
     switch (topic){
         case "payment":
             const paymentId = query.id || query['data.id'];
-            console.log(topic,"obteniendo payment : ",paymentId);
+            //console.log(topic,"obteniendo payment : ",paymentId);
             const payment = await mercadopago.payment.findById(Number(paymentId));
             console.log("PAYMENT-----------")
-            console.log(payment);
+            console.log("STATUS:::");
+            console.log(payment.body.status)
+            console.log("TRANSACTIONS DETAIL:::")
+            console.log(payment.body.transaction_details);
+            console.log(payment.body.transaction_details.net_received_amount);
             console.log("FIN PAYMENT-----------")
-            console.log(topic,"obteniendo merchand order");
-            const merchantOrder = await mercadopago.merchant_orders.findById(payment.body.order.id);
+            
+            if (payment.body.status==="approved"){
+                //registro en base de datos la donacion
+
+                //envio email con pdf
+                let contenidoPDF= `
+                    COMPROBANTE DE DONACION
+
+                    EMAIL DEL DONANTE: ${emailDonante}
+                    CAMPAÑA: ${campaignNombre}
+                    MONTO DONADO: ${payment.body.transaction_details.net_received_amount}
+                `
+                let pdf = await generatePDF(contenidoPDF);
+                try {
+                    await sendMailWithAttachment(emailDonante,
+                        "Comprobante de donacion",
+                        "Su comprobante se encuentra en el pdf adjunto",
+                        pdf,
+                        "comprobante.pdf");
+                } catch (error) {
+                    console.log("Falla de envio de mail en notificacionDonacionController")
+                    console.log(error);
+                }
+            }
+
+/*             console.log(topic,"obteniendo merchand order");
+            const merchantOrder = await mercadopago.merchant_orders.findById(payment.body.order.id); */
             break
     }
 
